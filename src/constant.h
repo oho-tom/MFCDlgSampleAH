0d01c3479c5ce160880f8128d3c3f70fbab3ebfa
tomoyuki_oho <>
2018-10-02 09:37:51 +0900

    * 定数定義用のヘッダファイルを追加。 * アプリケーションのディレクトリ取得失敗のメッセージを追加 * 多重起動抑止のメッセージを追加。 * ソリューションエクスプローラーのフィルタ名を変更。

---
+++ src/constant.h
@@ -0,0 +1,9 @@
+#ifndef MYAPP_CONSTANT_H_
+#define MYAPP_CONSTANT_H_
+
+const wchar_t* kAppName = L"MyApp";
+
+const wchar_t* kMessageFailToGetAppDirectory = L"アプリケーションのディレクトリ取得に失敗しました。";
+const wchar_t* kMessageAppDuplicated = L"本ツールは多重起動できません。";
+
+#endif
